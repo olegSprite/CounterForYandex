883880b9602d45609f5aa2b2418f4b12d3387c14
Oleg Spiridonov <spiri300@yandex.ru>
2023-10-15 11:07:02 +0500

    Добавлена история изменения с указанием даты

--- CounterForYandex/ViewController.swift
+++ CounterForYandex/ViewController.swift
@@ -15,8 +15,22 @@ class ViewController: UIViewController {
     @IBOutlet weak var plusButton: UIButton!
     @IBOutlet weak var minesButton: UIButton!
     @IBOutlet weak var historyLable: UITextView!
+    
     var counter: Int = 0
     
+    func takeTime() -> String {
+        let calendar = Calendar.current
+        let date = Date()
+        let curentTime = calendar.dateComponents([.month, .day, .hour, .minute, .second,], from: date)
+        
+        if let day = curentTime.day, let mounth = curentTime.month, let hour = curentTime.hour, let minute = curentTime.minute, let second = curentTime.second {
+            
+            return "[\(day).\(mounth) \(hour):\(minute):\(second)]"
+        }
+        
+        return "Дата не определенна"
+    }
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -34,6 +48,8 @@ class ViewController: UIViewController {
         
         counter = 0
         lable.text = "Значение счётчика: \(counter)"
+        let time = takeTime()
+        historyLable.text += "\n\n\(time): значение сброшено"
     
     }
     
@@ -41,6 +57,8 @@ class ViewController: UIViewController {
         
         counter += 1
         lable.text = "Значение счётчика: \(counter)"
+        let time = takeTime()
+        historyLable.text += "\n\n\(time): значение изменено на +1"
         
     }
     @IBAction func minesButtonTap(_ sender: Any) {
@@ -48,8 +66,13 @@ class ViewController: UIViewController {
         counter -= 1
         if counter < 0 {
             counter = 0
-        }
-        lable.text = "Значение счётчика: \(counter)"
+            lable.text = "Значение счётчика: \(counter)"
+            let time = takeTime()
+            historyLable.text += "\n\n\(time): попытка уменьшить значение счётчика ниже 0"
+        } else {
+            lable.text = "Значение счётчика: \(counter)"
+            let time = takeTime()
+            historyLable.text += "\n\n\(time): значение изменено на -1" }
         
     }
 }
