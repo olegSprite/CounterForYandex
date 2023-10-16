6f4be8737832e4f1da23ddaab50374b22976dd0f
Oleg Spiridonov <spiri300@yandex.ru>
2023-10-17 00:49:58 +0500

    Доработан код с рекомендацией после ревью, а именно
    В свойства и функции добавлена приватность
    Удалены комментарии
    переименнованно название элементов для понятного смысла
    Отступы
    порядок объявления свойств и функций

--- CounterForYandex/ViewController.swift
+++ CounterForYandex/ViewController.swift
@@ -1,25 +1,20 @@
-//
-//  ViewController.swift
-//  CounterForYandex
-//
-//  Created by Олег Спиридонов on 15.10.2023.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var button: UIButton!
-    @IBOutlet weak var lable: UILabel!
-    @IBOutlet weak var clearButton: UIButton!
-    @IBOutlet weak var plusButton: UIButton!
-    @IBOutlet weak var minesButton: UIButton!
-    @IBOutlet weak var historyLable: UITextView!
     
-    var counter: Int = 0
+    @IBOutlet weak private var button: UIButton!
+    @IBOutlet weak private var lable: UILabel!
+    @IBOutlet weak private var clearButton: UIButton!
+    @IBOutlet weak private var plusButton: UIButton!
+    @IBOutlet weak private var minesButton: UIButton!
+    @IBOutlet weak private var historyTextView: UITextView!
+    private var counter: Int = 0
     
-    // Функция получения текущего времени
-    func takeTime() -> String {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    private func takeTime() -> String {
         let calendar = Calendar.current
         let date = Date()
         let curentTime = calendar.dateComponents([.month, .day, .hour, .minute, .second,], from: date)
@@ -31,49 +26,44 @@ class ViewController: UIViewController {
         
         return "Дата не определенна"
     }
-   
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
     
     
-    @IBAction func buttonTap(_ sender: Any) {
-       
+    @IBAction private func buttonTap(_ sender: Any) {
+        
         counter += 1
         lable.text = "Значение счётчика: \(counter)"
         
     }
     
-    @IBAction func clearCount(_ sender: Any) {
+    @IBAction private func clearCount(_ sender: Any) {
         
         counter = 0
         lable.text = "Значение счётчика: \(counter)"
         let time = takeTime()
-        historyLable.text += "\n\n\(time): значение сброшено"
-    
+        historyTextView.text += "\n\n\(time): значение сброшено"
+        
     }
     
-    @IBAction func plusButtonTap(_ sender: Any) {
+    @IBAction private func plusButtonTap(_ sender: Any) {
         
         counter += 1
         lable.text = "Значение счётчика: \(counter)"
         let time = takeTime()
-        historyLable.text += "\n\n\(time): значение изменено на +1"
+        historyTextView.text += "\n\n\(time): значение изменено на +1"
         
     }
-    @IBAction func minesButtonTap(_ sender: Any) {
+    @IBAction private func minesButtonTap(_ sender: Any) {
         
         counter -= 1
         if counter < 0 {
             counter = 0
             lable.text = "Значение счётчика: \(counter)"
             let time = takeTime()
-            historyLable.text += "\n\n\(time): попытка уменьшить значение счётчика ниже 0"
+            historyTextView.text += "\n\n\(time): попытка уменьшить значение счётчика ниже 0"
         } else {
             lable.text = "Значение счётчика: \(counter)"
             let time = takeTime()
-            historyLable.text += "\n\n\(time): значение изменено на -1" }
+            historyTextView.text += "\n\n\(time): значение изменено на -1" }
         
     }
 }
